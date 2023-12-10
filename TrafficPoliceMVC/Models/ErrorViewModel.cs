f20516a4c01f537b646169e15c7b2e328a72ca0b
Farkhadzz <zulfugarovfarhad6@gmail.com>
2023-12-10 17:24:31 +0400

    Created TrafficPoliceMVC App

---
+++ TrafficPoliceMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TrafficPoliceMVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
