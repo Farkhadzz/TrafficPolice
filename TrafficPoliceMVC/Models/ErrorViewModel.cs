51f4b93fd8d523e3d8244e7edb26378d51df72f4
Farkhadzz <zulfugarovfarhad6@gmail.com>
2024-01-21 00:27:58 +0400

    Fixed some problems

--- TrafficPoliceMVC/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace TrafficPoliceMVC.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
